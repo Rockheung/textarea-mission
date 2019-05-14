a5800d8156aba4910a87ed4aa8b1a81baf3f827b
Rockheung <rockheung@gmail.com>
2019-05-14 04:33:02 +0000

    Init react app package with reactstrap

--- back/mdl/index.js
+++ back/mdl/index.js
@@ -8,6 +8,7 @@ const dbName = 'webCoder';
 
 let _db = null;
 // Initialize connection once
+// 더 좋은 방법으로는 module.exports = client를 하여 ctrlr/index.js의 handler을 통해 db를 전달하는 방법이 있을 것 같습니다.
 exports.connect = async () => {
 	let client = new MongoClient(url, { useNewUrlParser: true });
 	try {
